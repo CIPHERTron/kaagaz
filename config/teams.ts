6bd2661ab4a7eebb70aeccd9cfda28e2cdbc3910
CIPHERTron <pritish.samal918@gmail.com>
2021-10-07 14:19:17 +0530

    feat: add what we do component

--- config/teams.ts
+++ config/teams.ts
@@ -1,3 +1,6 @@
+export const aboutContent =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum integer enim neque volutpat ac tincidunt vitae. Nisl nisi scelerisque eu ultrices. Dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu. Euismod in pellentesque massa placerat duis ultricies lacus sed turpis. Blandit massa enim nec dui nunc mattis. Tincidunt id aliquet risus feugiat in. Ornare suspendisse sed nisi lacus sed viverra tellus. Quam elementum pulvinar etiam non quam. Nulla malesuada pellentesque elit eget. Arcu odio ut sem nulla pharetra diam sit. Quisque non tellus orci ac auctor augue mauris augue.';
+
 export const eighteen = [
   {
     name: 'John Doe',
