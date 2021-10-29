40ffbdba19f7b23db0e99906a9e9fa1e3fecefe8
CIPHERTron <pritish.samal918@gmail.com>
2021-10-30 01:54:23 +0530

    feat: Display Credits in Legacy Card

--- components/LegacyCard.tsx
+++ components/LegacyCard.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import Link from 'next/link';
-import { Wrapper, Container, Image, ArticleHeading, Quote, Anchor } from '../styles/legacy.styles';
+import {
+  Wrapper,
+  Container,
+  Image,
+  ArticleHeading,
+  Quote,
+  Anchor,
+  Credits,
+  Attribute,
+} from '../styles/legacy.styles';
 
 function LegacyCard({ article }) {
-  const { date, description, slug, title } = article;
+  const { description, slug, title, content, design, category } = article;
   return (
     <Wrapper>
       <Container>
         <ArticleHeading>{title}</ArticleHeading>
         <Quote>{description}</Quote>
+        <Credits>
+          <Attribute>Content: {content}</Attribute>
+          <Attribute>Design: {design}</Attribute>
+        </Credits>
         <Link href={`/articles/${slug}`}>
           <Anchor rel='noreferrer noopener'>Read more...</Anchor>
         </Link>
