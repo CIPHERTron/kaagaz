b3caead0e2ee72a230cb7c6a77e7cabc0b8e6b21
Ratnakar <ratnakar3895@yahoo.com>
2021-10-30 21:50:47 +0530

    fix: height abnormality in articles

--- styles/legacy.styles.ts
+++ styles/legacy.styles.ts
@@ -4,7 +4,7 @@ export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   background-color: #f0f0f0;
-  height: 545px;
+  height: auto;
   width: 1080px;
   align-items: center;
   margin: 3% auto;
