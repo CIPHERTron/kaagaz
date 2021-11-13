5d6e2a322db18c33fd5907f2b7cd07aea9330821
CIPHERTron <pritish.samal918@gmail.com>
2021-11-13 13:48:03 +0530

    feat: Add Editor's Desk Articles

--- components/EditorDesk.tsx
+++ components/EditorDesk.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { Container } from '../styles/editor.styles';
 import EditorDeskCard from './EditorDeskCard';
 
+import { editorsDesk } from '../config/index.json';
+
 function EditorDesk() {
   return (
     <Container>
-      <EditorDeskCard />
-      <EditorDeskCard />
-      <EditorDeskCard />
-      <EditorDeskCard />
+      {editorsDesk.map(({ image, alt, link, title, content, credit1, credit2 }) => (
+        <EditorDeskCard
+          image={image}
+          alt={alt}
+          link={link}
+          title={title}
+          content={content}
+          credit1={credit1}
+          credit2={credit2}
+        />
+      ))}
     </Container>
   );
 }
