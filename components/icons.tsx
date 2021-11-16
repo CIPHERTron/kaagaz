35bf9d4f136160a8a35b377a94ed2ff3b3811c18
Ratnakar <ratnakar3895@yahoo.com>
2021-11-16 19:32:48 +0530

    fix: footer data

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,29 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
-import { Twitter, Instagram, Linkedin, Mail } from 'react-feather';
+import { Twitter, Instagram, Linkedin, Mail, Facebook } from 'react-feather';
 import { Wrapper } from '../styles/icons.styles';
 
 export const Icons = () => {
   return (
     <Wrapper>
-      <Link href='https://linkedin.com/in/'>
+      <Link href='https://m.facebook.com/shout.d361/'>
         <a target='_blank' rel='noreferrer noopener' className='icon-link'>
-          <Linkedin />
+          <Facebook />
         </a>
       </Link>
       <Link href='https://instagram.com/d361official/'>
         <a target='_blank' rel='noreferrer noopener' className='icon-link'>
           <Instagram />
-        </a>
-      </Link>
-      <Link href='https://twitter.com/'>
-        <a target='_blank' rel='noreferrer noopener' className='icon-link'>
-          <Twitter />
-        </a>
-      </Link>
-      <Link href='mailto:random@gmail.com'>
-        <a target='_blank' rel='noreferrer noopener' className='icon-link'>
-          <Mail />
         </a>
       </Link>
     </Wrapper>
