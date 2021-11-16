1db1c80a66d7b837b9af073912bde4142af4a074
CIPHERTron <pritish.samal918@gmail.com>
2021-11-17 02:48:07 +0530

    fix: footer links and spacing

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
-import { Twitter, Instagram, Linkedin, Mail, Facebook } from 'react-feather';
+import { Instagram, Linkedin, Mail, Facebook } from 'react-feather';
 import { Wrapper } from '../styles/icons.styles';
 
 export const Icons = () => {
   return (
     <Wrapper>
-      <Link href='https://m.facebook.com/shout.d361/'>
-        <a target='_blank' rel='noreferrer noopener' className='icon-link'>
-          <Facebook />
-        </a>
-      </Link>
       <Link href='https://instagram.com/d361official/'>
         <a target='_blank' rel='noreferrer noopener' className='icon-link'>
           <Instagram />
+        </a>
+      </Link>
+      <Link href='mailto:xpress.d361@gmail.com'>
+        <a target='_blank' rel='noreferrer noopener' className='icon-link'>
+          <Mail />
+        </a>
+      </Link>
+      <Link href='https://m.facebook.com/shout.d361/'>
+        <a target='_blank' rel='noreferrer noopener' className='icon-link'>
+          <Facebook />
         </a>
       </Link>
     </Wrapper>
