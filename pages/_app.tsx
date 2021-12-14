5f00039cbb987bcfd91857a574796c8b0c103416
CIPHERTron <pritish.samal918@gmail.com>
2021-12-14 13:51:56 +0530

    chore: Add Google Analytics | bump: net from 10 to 11

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 
 import '../styles/layout.css';
 
@@ -8,7 +9,28 @@ import '../styles/layout.css';
  * @param {object} pageProps All props for the page
  */
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=G-7CM06GHEVY`}
+      />
+
+      <Script strategy='lazyOnload'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){
+            dataLayer.push(arguments);            
+          }
+          gtag('js', new Date());
+
+          gtag('config', 'G-7CM06GHEVY');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
