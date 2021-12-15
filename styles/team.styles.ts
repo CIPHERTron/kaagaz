5c6aaa9c3cb66d9b64101778a145189d28902908
Ratnakar <ratnakar3895@yahoo.com>
2021-12-15 09:47:27 +0530

    feat: added 2021-22 Team page

--- styles/team.styles.ts
+++ styles/team.styles.ts
@@ -32,7 +32,7 @@ export const Heading = styled.h3`
 
 export const ButtonContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   margin-top: 8px;
 `;
 
